1cfe81c9a3632b961fdd0ec1fc74cf1c15e705b7
RayBi13 <binturaoul@gmail.com>
2025-02-12 19:24:13 +0100

    Exercices Day 3

--- CrashCourse/Week1/Day3/Exo_4.py
+++ CrashCourse/Week1/Day3/Exo_4.py
@@ -1,37 +1,25 @@
 class Zoo:
-    def __init__(self, name):
-      self.name = name
-      self.animal = []
-
+    def __init__ (self, zoo_name):
+        self.name = zoo_name
+        self.animals = []
     def add_animal(self, new_animal):
-       if new_animal not in self.new_animal:
-           self.animal.append(new_animal)
-
-
+        if new_animal not in self.animals:
+            self.animals.append(new_animal)      
     def get_animals(self):
-        print(self.animal)
-
+        print (self.animals)
     def sell_animal(self, animal_sold):
-        self.animal.remove(animal_sold)
-        
-
+        self.animals.remove(animal_sold)
     def sort_animals(self):
-        self.animal.sort()
-
-        def sort_animals(animals):
-            sorted_animals = sorted(animals)
-    grouped_animals = {}
-    
-    for animal in sorted_animals:
-        first_letter = animal[0].upper()
-        if first_letter not in grouped_animals:
-            grouped_animals[first_letter] = []
-        grouped_animals[first_letter].append(animal)
-    
-     return grouped_animals
-
-    def main():
-            animals = ["elephant", "dog", "cat", "antelope", "bear", "alligator", "cheetah"]
-            grouped_animals = sort_animals(animals)
-    for letter, group in grouped_animals.items():
-        print(f"{letter}: {group}")
+        self.animals.sort()
+        grouped_animals = {}
+        for animal in self.animals:
+            first_letter = animal[0].upper()
+            if first_letter not in grouped_animals:
+                grouped_animals[first_letter] = []
+            grouped_animals[first_letter].append(animal)
+        return grouped_animals
+zoo = Zoo("my_zoo")
+animals_list = ["Zebra", "Antelope", "Lion", "Tiger", "Leopard", "Elephant", "Giraffe"]
+for animal in animals_list:
+    zoo.add_animal(animal)
+print(zoo.sort_animals())
